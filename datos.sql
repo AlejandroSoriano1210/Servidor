d8ef4fb58f1c88bc5984427438eb4e2792ebefe7
AlexandroSoriano <alejandro.soriano@iesdonana.org>
2024-10-29 19:34:09 +0100

    Actualizado

--- datos.sql
+++ datos.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS empleado CASCADE ;
+
 DROP TABLE IF EXISTS departamentos CASCADE ;
 
 CREATE TABLE departamentos (
@@ -7,9 +9,26 @@ CREATE TABLE departamentos (
     localidad    VARCHAR(255) 
 );
 
+CREATE TABLE empleado(
+    id                  BIGSERIAL    PRIMARY KEY NOT NULL,
+    numero              VARCHAR(255) NOT NULL UNIQUE,
+    nombre              VARCHAR(255) NOT NULL,
+    apellidos           VARCHAR(255) NOT NULL,
+    departamento_id     BIGINT       NOT NULL
+                                     REFERENCES departamentos (id)
+);
+
+
 -------------------
 
 INSERT INTO departamentos (codigo, denominacion, localidad)
 VALUES ('10', 'Informática', 'Sanlúcar'),
        ('20', 'Administrativo', NULL),
        ('30', 'Matemáticas', 'Chipiona');
+
+-------------------
+
+INSERT INTO empleado (numero, nombre, apellidos, departamento_id)
+VALUES ('626', 'Pedro', 'OxSmall Pérez', '1'),
+       ('172', 'Juanjo', 'Melan Sáncchez', '3'),
+       ('554', 'Gustavo', 'Melame Lamano', '2');
---
+++ empleados.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $pdo = new PDO('pgsql:host=localhost;dbname=datos', 'datos', 'hnmpl');
+    $stmt = $pdo->query('SELECT e.*, d.codigo, d.denominacion, d.localidad 
+                           FROM empleado e JOIN departamentos d
+                             ON e.departamento_id=d.id
+                       ORDER BY numero');
+    ?>
+    <table border="1">
+        <thead>
+            <th>Número</th>
+            <th>Nombre</th>
+            <th>Apellidos</th>
+            <th>Código Departamento</th>
+            <th>Departamento</th>
+        </thead>
+        <tbody>
+            <?php foreach ($stmt as $fila):  ?>
+                <tr>
+                    <td><?= $fila['numero']?></td>
+                    <td><?= $fila['nombre']?></td>
+                    <td><?= $fila['apellidos']?></td>
+                    <td><?= $fila['codigo']?></td>
+                    <td><?= $fila['denominacion']?></td>
+                </tr>
+            <?php   endforeach   ?>
+        </tbody>
+    </table>
+</body>
+</html>
